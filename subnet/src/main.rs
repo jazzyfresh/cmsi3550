0961301ab5c5f12a61a983e10f4ec7c6ed11d460
Jazzy <jasmine.dahilig@gmail.com>
2024-02-19 15:23:49 -0800

    added split string for target ip and cidr

--- subnet/src/main.rs
+++ subnet/src/main.rs
@@ -1,12 +1,18 @@
 use std::env::args;
 
 fn main() {
-    // Get operating system args passed into this program
-    let subnet_args: Vec<String> = args().collect();
+    // // Get operating system args passed into this program
+    // let subnet_args: Vec<String> = args().collect();
 
     // Iterate through args with index
-    for (i, arg) in subnet_args.iter().enumerate() {
+    for (i, arg) in args().enumerate() {
         println!("Arg {i}: {arg}");
+
+        // subnet_args[0] is the name of the executable
+        // subnet_args[1] the target ip address + cidr notation
+        if i == 1 {
+            calculate_subnet(arg);
+        }
     }
 
     // // args() gives you an iterator already
@@ -14,4 +20,22 @@ fn main() {
     // for (i, arg) in args().enumerate() {
     //     println!("Arg {i}: {arg}");
     // }
+}
+
+fn calculate_subnet(target_subnet: String) {
+    // how to split strings
+    let parts = target_subnet.split("/");
+    for part in parts {
+        println!("Subnet part: {part}");
+    }
+
+    // // how to split strings into named variables
+    // let [target_ip, cidr]: [&str; 2] = target_subnet
+    //     .split("/")
+    //     .collect::<Vec<&str>>()
+    //     .try_into()
+    //     .unwrap_or_default();
+    //
+    // println!("Target IP: {target_ip}");
+    // println!("CIDR: {cidr}");
 }
